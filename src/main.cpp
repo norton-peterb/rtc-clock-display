2c61e82e9b21138357a6105e3b0556d35b57da1f
Peter B Norton <norton.peterb@gmail.com>
2022-12-06 21:42:37 +0000

    Interface RTC Module

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,14 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include "SparkFunDS1307RTC.h"
+
+char buffer[24];
 
 void setup() {
-  
+  rtc.begin();
+  rtc.set24Hour(true);
 }
 
 void loop() {
-  
+  delay(1000);
 }
